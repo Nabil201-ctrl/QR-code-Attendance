6443ffc6b2c633b2e8c1e03d8669a664044b77fc
Nabil201-ctrl <abubakar.nabil.210@gmail.com>
2025-11-15 20:31:10 +0100

    (feature): added .env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AttendanceModule } from './attendance/attendance.module';
+import { HealthModule } from '../health/health.module';
 import { AdminModule } from './admin/admin.module';
+import { AttendanceModule } from './attendance/attendance.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb+srv://abubakarnabil:admin123@qrcode.atvxjaa.mongodb.net/?appName=qrcode'),
     AttendanceModule,
     AdminModule,
+    HealthModule
   ],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const port = 4000;
   const app = await NestFactory.create(AppModule);
-
+   const serverUrl = process.env.SERVER_URL 
+   
   app.enableCors({
     origin: true, // Allow all origins in development
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
@@ -17,5 +18,14 @@ async function bootstrap() {
   
   await app.listen(process.env.PORT ?? port);
   console.log(`🚀 Server running on port ${port}`);
+    // Automatic health ping every 4 minutes
+  setInterval(async () => {
+    try {
+      await fetch(`${serverUrl}/health`);
+      console.log("🔄 Server pinged /health");
+    } catch (err) {
+      console.log("❌ Failed to ping /health:", err.message);
+    }
+  }, 4 * 60 * 1000); // every 4 minutes
 }
 bootstrap();
